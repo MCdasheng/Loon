866e70471e0b66c48da7bc94c4818d27a0442d7e
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2022-11-20 01:36:05 +0800

    更新 Testflight.js

--- Scripts/Testflight.js
+++ Scripts/Testflight.js
@@ -13,7 +13,7 @@ const $ = new Env('TestFilght监控');
  * 填入要监测的appkey。从testfligt地址获取。
  * 例如"VCIvwk2g,wArXdacJ,2vnRvOTX,LzjySbQx,IdFRwmNy,qDkBu2ur,4Qt2lIm5,ZzqOu8tX,ftCqFe6D,fy7LvHVA,QKqitFwc"
 */
-const appkey = "VCIvwk2g,wArXdacJ,2vnRvOTX,LzjySbQx,IdFRwmNy,qDkBu2ur,4Qt2lIm5,ZzqOu8tX,ftCqFe6D,fy7LvHVA,QKqitFwc"";
+const appkey = "VCIvwk2g,wArXdacJ,2vnRvOTX,LzjySbQx,IdFRwmNy,qDkBu2ur,4Qt2lIm5,ZzqOu8tX,ftCqFe6D,fy7LvHVA,QKqitFwc";
 //const appkey = $.getdata('appkey');
 
 //是否在没有tf位置的时候仍然弹出通知，默认不弹出,防止过多无用通知。
