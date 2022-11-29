3449487d9d17d6265246e02e478c1934c3366510
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2022-11-29 16:09:39 +0800

    Create YaNetflix.js

---
+++ Scripts/YaNetflix.js
@@ -0,0 +1,19 @@
+/***
+自用备份 鸭奈飞去广告-Eric
+# url = https://raw.githubusercontent.com/MCdasheng/Module/main/Scripts/YaNetflix.js 
+[rewrite_local]
+^http://pipi.4kya.com//xgapp.php/v3/advert url script-response-body https://raw.githubusercontent.com/MCdasheng/Module/main/Scripts/YaNetflix.js 
+[mitm] 
+hostname = %APPEND% pipi.4kya.com
+***/
+
+
+let obj = JSON.parse($response.body);
+
+obj = {
+  "msg": "广告列表",
+  "data": [
+  ],
+  "code": 1
+}
+$done({body: JSON.stringify(obj)});
