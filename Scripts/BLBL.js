5767ff0c09a1d4cd2dab34ea3335563c6293142f
MCdasheng <92009557+MCdasheng@users.noreply.github.com>
2023-01-03 11:53:04 +0800

    Update BLBL.js

--- Scripts/BLBL.js
+++ Scripts/BLBL.js
@@ -7,7 +7,6 @@ QX:
 [rewrite_local]
 #哔哩哔哩解锁大会员
 ^http[s]?:\/\/((app|api)\.(\w{2,15})?\.(com|cn)).*player\.(v3|v2|v1).Play(URL|View).*$ url script-request-header https://raw.githubusercontent.com/MCdasheng/Module/main/Scripts/BLBL.js
-
 [Mitm]
 hostname = *.biliapi.*, *.bilibili.*
 -----------------------------------------------------------------------------------------------------------------------------------------------------
